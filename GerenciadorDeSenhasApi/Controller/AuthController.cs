64c9fd7258b919a77ae8357b82f05dc178c4e885
ferbarcante <barcantef@gmail.com>
2025-05-08 18:50:10 -0300

    verificacao

--- GerenciadorDeSenhasApi/Controller/AuthController.cs
+++ GerenciadorDeSenhasApi/Controller/AuthController.cs
@@ -23,4 +23,22 @@ public class AuthController : ControllerBase
 
         return Ok(user);
     }
+    
+    [HttpPost("login")]
+    public ActionResult<string> Login(UserDto request)
+    {
+        if (user.Username != request.Username)
+        {
+            return BadRequest("User not found");
+        }
+
+        if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+            == PasswordVerificationResult.Failed)
+        {
+            return BadRequest("Wrong password");
+        }
+
+        string token = "sucess";
+        return Ok(token);
+    }
 }
